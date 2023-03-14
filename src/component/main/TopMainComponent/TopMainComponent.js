bfedcc2f67f190fb606959b78f335c6c2ef5f6d0
michael menebraye <michael@dufuna.org>
2023-03-14 07:02:21 +0100

    succueesfully completed the dashboard component, remaining the navs

--- src/component/main/TopMainComponent/TopMainComponent.js
+++ src/component/main/TopMainComponent/TopMainComponent.js
@@ -6,15 +6,15 @@ import TopLeftCardComponent from "./sub_folder/TopLeftCardComponent";
 const TopMaincomponent = () => {
     return (
         <div className="container-fluid overflow-hidden mt-1">
-            <div className="row gy-5 ">
+            <div className="row gy-3 ">
                 <TopLeftCardComponent/>
                 
-                <div className="col-md-8 overflow-auto col-lg-6">
+                <div className="col-md-5 overflow-auto col-lg-6">
                     <div className="p-3 rounded  align-self-center ">
                         <ChartComponent />
                     </div>
                 </div>
-                <div className="col-md-4 col-lg-3 ">
+                <div className="col-md-3 col-lg-3 ">
                     <TopCardComponent />
 
                 </div>
